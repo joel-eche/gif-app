8120c41b4cf6195a82fcd414b31fc33b2287412b
joeleche <joel.eche5@gmail.com>
2020-09-19 20:13:21 -0500

    Rename build file to docs

---
+++ docs/precache-manifest.fe5c93867925cd5161e70bd7287e7aac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9f2d6ca17225bc2cc21c98a039305a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8686ecbbe598e77a9f4",
+    "url": "/static/css/main.78fea000.chunk.css"
+  },
+  {
+    "revision": "853c34407d511095cb26",
+    "url": "/static/js/2.d93bb89d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d93bb89d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8686ecbbe598e77a9f4",
+    "url": "/static/js/main.b85d110d.chunk.js"
+  },
+  {
+    "revision": "3d997354fbefbeed772c",
+    "url": "/static/js/runtime-main.7aabfece.js"
+  }
+]);

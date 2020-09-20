0e32af32bf90f3b3fc41d1423b065b0bf1706181
joeleche <joel.eche5@gmail.com>
2020-09-19 20:19:29 -0500

    change url to https

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=5&api_key=y4OVO9PrrtKq9HKH0pLRCXFfS5C7Hy73`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=5&api_key=y4OVO9PrrtKq9HKH0pLRCXFfS5C7Hy73`;
   const res = await fetch(url);
   const { data } = await res.json();
   const imagesResult = data.map(element => ({
